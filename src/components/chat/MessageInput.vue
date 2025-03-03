bd9f2646aefe6e66a80e570eeb49a46536aa8a63
ChristianHopf <cehopf@gmail.com>
2025-03-03 06:18:16 -0500

    Add send button

--- src/components/chat/MessageInput.vue
+++ src/components/chat/MessageInput.vue
@@ -2,7 +2,8 @@
 </script>
 
 <template>
-  <div class="">
-    <input type="text" class="w-full border-2 border-blue-600 rounded-md">
+  <div class="flex flex-row gap-4">
+    <input type="text" class="w-full border-2 border-blue-600 rounded-md" />
+    <button type="button" class="text-white bg-blue-600 px-2 py-1 rounded-md">Send</button>
   </div>
 </template>
--- src/components/ui/TheHeader.vue
+++ src/components/ui/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="flex max-w-full justify-between bg-white px-4 py-2">
+  <header class="flex max-w-full justify-between px-4 py-2 bg-white">
     <h1 class="text-3xl franklin-gothic italic text-blue-600">Vox</h1>
     <div class="flex gap-4">
       <button>Action 1</button>
