09f75c86da3f1cbad7f22a06f5239b9c1deffbab
ChristianHopf <cehopf@gmail.com>
2025-03-03 09:07:49 -0500

    modal for adding server

---
+++ src/components/modals/TheModal.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="absolute flex justify-center items-center min-h-screen w-full bg-black opacity-50">
+  </div>
+  <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col
+    items-center gap-2 bg-blue-200 text-center opacity-100 px-4 py-2 rounded-md">
+    <h2 class="m-0">Address</h2>
+    <input type="text" class="bg-white border-2 border-blue-600 rounded-md"/>
+    <button type="button" class="text-white bg-blue-600 px-2 py-1 rounded-md">Add</button>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,13 @@
 import Messages from '@/components/chat/Messages.vue';
 import MessageInput from '@/components/chat/MessageInput.vue';
 import TheSidebar from '@/components/sidebar/TheSidebar.vue';
+import TheModal from '@/components/modals/TheModal.vue';
 
 </script>
 
 <template>
   <main class="flex w-full">
+    <TheModal />
     <TheSidebar />
     <div
       class="flex flex-col w-full px-4 py-2 border-s-2 border-t-2 border-b-2 bg-white border-blue-600 rounded-s-3xl">
