ed5b088dfdce872a4741ad9ffaafeff1fcc11fd0
ChristianHopf <cehopf@gmail.com>
2025-03-05 10:02:37 -0500

    Connect socket to url, emit connect event

---
+++ client/src/util/api/api.ts
@@ -0,0 +1,16 @@
+import { io, Socket } from "socket.io-client";
+
+interface Config {
+  server: string;
+  port: number;
+  nick: string;
+}
+
+const url = "http://localhost:3000";
+let socket: Socket | null = null;
+
+export function connectToServer(config: Config) {
+  socket = io(url);
+
+  socket.emit('connect-to-server', config);
+}
