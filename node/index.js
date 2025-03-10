cfce240f0d766eaa153cac61cef7b698f0dfdc25
ChristianHopf <cehopf@gmail.com>
2025-03-10 13:21:43 -0400

    call connect properly, listen for registered

--- node/index.js
+++ node/index.js
@@ -30,34 +30,19 @@ io.on("connection", (socket) => {
   console.log("Client connected to:", socket.id);
   let ircClient = null;
 
-  socket.on("connect-to-server", ({ server: address, port, nick }) => {
+  socket.on("connect-to-server", ({ address, port, nick }) => {
     ircClient = new irc.Client(address, nick, {
       port: port || 6667,
       autoConnect: false,
     });
-    // On connect, emit successful connect
-    ircClient.connect(() => {
-      socket.emit("status", "Connected to server");
-    });
 
-    // On registered, emit successfully registered
-    ircClient.on("registered", () => {
-      socket.emit("status", "Successfully registered on server");
-    });
+    socket.emit("status", "Connecting to ${address}:${port}...");
+    ircClient.connect();
+  });
 
-    // On receiving a message, emit it to the appropriate channel
-    ircClient.on("message", (from, to, message) => {
-      socket.emit("message", {
-        from,
-        message,
-        channel: to,
-      });
-    });
-
-    // On error err, emit err
-    ircClient.on("error", (err) => {
-      socket.emit("error", err);
-    });
+  // On registered, emit successfully registered
+  ircClient.on("registered", () => {
+    socket.emit("status", "Successfully registered on server");
   });
 
   socket.on("send-message", ({ channel, message }) => {
@@ -75,6 +60,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
+io.listen(3000, () => {
   console.log("Listening on port 3000");
 });
